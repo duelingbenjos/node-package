9d84e04c9e7e56d96dacc8204635af00edc36b16
Nikita Tokariev <mykyta.tokariev@gmail.com>
2023-02-12 11:22:50 -0500

    utilize asyncio.create_subprocess_exec

--- manager.py
+++ manager.py
@@ -3,7 +3,11 @@ from dateutil import parser
 import asyncio
 import os
 import socketio
-import subprocess
+
+async def run_command(args):
+    process = await asyncio.create_subprocess_exec(*args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+    return await process.wait()
 
 async def upgrade_handler(data: dict):
     os.environ['LAMDEN_TAG'] = data['lamden_tag']
@@ -12,29 +16,29 @@ async def upgrade_handler(data: dict):
     os.environ.pop('LAMDEN_NETWORK', None)
     utc_when = parser.parse(data['utc_when'])
 
-    if subprocess.call(['make', 'build']) != 0:
+    if await run_command(['make', 'build']) != 0:
         return
 
     while utc_when > datetime.utcnow():
         await asyncio.sleep(1)
 
-    subprocess.call(['make', 'restart'])
+    await run_command(['make', 'restart'])
 
 async def network_error_handler(data: dict):
-    if subprocess.call(['make', 'stop']) != 0:
+    if await run_command(['make', 'stop']) != 0:
         return
 
     network_is_down = True
     while network_is_down:
         for ip in data['bootnode_ips']:
-            if subprocess.call(['ping', '-c', '1', ip]) == 0:
+            if await run_command(['ping', '-c', '1', ip]) == 0:
                 network_is_down = False
                 break
 
     os.environ['LAMDEN_BOOTNODES'] = ':'.join(data['bootnode_ips'])
     os.environ.pop('LAMDEN_NETWORK', None)
 
-    subprocess.call(['make', 'start'])
+    await run_command(['make', 'start'])
 
 event_handlers = {
     'upgrade': upgrade_handler,
@@ -55,11 +59,11 @@ async def disconnect():
 
 @sio.event
 async def event(event: dict):
-    await event_handlers[event['event']](event['data'])
+    asyncio.create_task(event_handlers[event['event']](event['data']))
 
 async def main():
     await sio.connect(f'http://localhost:17080')
     await sio.wait()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
